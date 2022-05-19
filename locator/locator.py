cf5b1d98c32befe63bf3afd1006c4580befd472b
shawn <shawn310480@gmail.com>
2022-05-19 16:34:22 +0800

    Fix

--- locator/locator.py
+++ locator/locator.py
@@ -1,3 +1,5 @@
-DRIVER_PATH = './../chromedriver'
+import os
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '../'))
+DRIVER_PATH = BASE_DIR + "/chromedriver"
 GOOGLE_INPUT_FIELD = '//input[@name="q"]'
 GOOGLE_SEARCH_BTN = '//input[@name="btnK"]'
--- testcase/init.robot
+++ testcase/init.robot
@@ -1,6 +1,6 @@
 *** Setting ***
 Library       Selenium2Library    30    10
 
-Library       ./../library/keywords_common.robot
+Resource       ./../library/keywords_common.robot
 
 Variables     ./../locator/locator.py
